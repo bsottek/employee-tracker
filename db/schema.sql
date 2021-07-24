7bfd87694f1d3f020cd128e83bcdb8321b6bedd7
bsottek <bsottek@gmail.com>
2021-07-23 23:17:11 -0500

    Constructor and create/view functions for department

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ DROP TABLE if EXISTS department;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
