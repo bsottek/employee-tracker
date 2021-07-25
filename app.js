0e742bda809b27eb1200e26076687c7792d794d9
bsottek <bsottek@gmail.com>
2021-07-25 02:29:16 -0500

    Prompts and actions for some prompt responses: view depts, view roles, view employees, add dept, add role

--- app.js
+++ app.js
@@ -4,6 +4,123 @@ const { Role, createRole, viewAllRoles } = require('./lib/Role');
 const { Department, createDepartment, viewAllDepartments } = require('./lib/Department');
 const cTable = require('console.table');
 
+const promptUser = () => {
+    console.log(`
+    ================================
+    Welcome to the Employee Tracker
+    ================================
+    `);
+
+    return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'actionSelection',
+                message: 'What would you like to do?',
+                choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit']
+            }
+        ])
+        .then(actionSelection => {
+            choiceHandler(actionSelection);
+        })
+}
+
+const choiceHandler = (({ actionSelection }) => {
+    switch (actionSelection) {
+        case 'View all departments':
+            viewAllDepartments()
+                .then(rows => {
+                    return console.table(rows);
+                })
+                .then(() =>{
+                    promptUser();
+                });
+            break;
+        case 'View all roles':
+            viewAllRoles()
+                .then(rows => {
+                    return console.table(rows);
+                })
+                .then(() => {
+                    promptUser();
+                });
+            break;
+        case 'View all employees':
+            viewAllEmployees()
+                .then(rows => {
+                    return console.table(rows);
+                })
+                .then(() => {
+                    promptUser();
+                });
+            break;
+        case 'Add a department':
+            createDepartmentPrompt()
+                .then(details => {
+                    createDepartment(details)
+                        .then(() => {
+                            promptUser();
+                        });
+                });
+            break;
+        case 'Add a role':
+            createRolePrompt()
+                .then(details => {
+                    createRole(details)
+                        .then(() => {
+                            promptUser();
+                        });
+                });
+            break;
+    }
+})
+
+const createDepartmentPrompt = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What will the new department be called?'
+
+            },
+        ])
+        .then(details => {
+            return details;
+        })
+}
+
+const createRolePrompt = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What will the new role be called?'
+
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary of this role?'
+            },
+            {
+                type: 'input',
+                name: 'departmentId',
+                message: 'Which department will this role fall under? (Please enter department ID)',
+            }
+        ])
+        .then(details => {
+            return details;
+        })
+}
+
+promptUser();
+
+
+
+// Uncomment below to test constructors and sql functions.
+
 // let newRole = new Role('Paid Media Strategist', '70000', 1);
 
 // createRole(newRole);
@@ -19,16 +136,19 @@ const cTable = require('console.table');
 
 // viewAllDepartments()
 //     .then(rows => {
-//         console.table(rows);
+//         let departments = rows;
+//         let departmentNames = []
+//         departments.forEach(department => departmentNames.push(department.department_name));
+//         return departmentNames;
 //     });
 
-let newEmployee = new Employee('Bob', 'Sagget', 1, null);
+// let newEmployee = new Employee('Bob', 'Sagget', 1, null);
 
-createEmployee(newEmployee);
+// createEmployee(newEmployee);
 
-updateEmployeeRole(2,9);
+// updateEmployeeRole(2,9);
 
-viewAllEmployees()
-    .then(rows => {
-        console.table(rows);
-    });
+// viewAllEmployees()
+//     .then(rows => {
+//         console.table(rows);
+//     });
